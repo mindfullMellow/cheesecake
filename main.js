3e6085076d2a1f9bc563c932b5308fe9384c3169
Daniel Samuel <samueldaniel4198@gmail.com>
2025-05-31 05:19:38 +0100

    New: added the scrolling animation wth js

--- main.js
+++ main.js
@@ -5,3 +5,30 @@ import "tailwindcss/tailwind.css";
 const currentYear = new Date().getFullYear();
 const year = document.querySelector("#year");
 year.textContent = currentYear;
+
+/////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+
+    // Prevent default ONLY for in-page scrolling links
+    if (href.startsWith("#")) {
+      e.preventDefault();
+
+      // Scroll to top if href is "#"
+      if (href === "#") {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      } else {
+        // Scroll to specific section
+        const sectionEl = document.querySelector(href);
+        if (sectionEl) sectionEl.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("item")) headerEl.classList.toggle("nav-open");
+  });
+});
